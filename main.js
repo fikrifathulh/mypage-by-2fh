429d496d07d0c026bb96e70b1d9bd1c17d61a5c8
fikrifathulh <145629171+fikrifathulh@users.noreply.github.com>
2024-11-20 22:25:06 +0700

    now main.js will import the components

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { App } from './App.js';
+import { Header } from './components/Header.js';
+import { Main } from './components/Main.js';
+import { Footer } from './components/Footer.js';
 
-const root = document.getElementById('root');
-if (root) {
-  root.appendChild(App());
-} else {
-  alert('Not Found');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.querySelector('#app');
+  app.append(Header(), Main(), Footer());
+});
