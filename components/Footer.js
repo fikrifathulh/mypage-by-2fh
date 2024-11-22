d72f2495e3c0be3890a8fd6e28d9705a76e77205
fikrifathulh <145629171+fikrifathulh@users.noreply.github.com>
2024-11-22 21:15:14 +0700

    updating footer content

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,15 @@ export const Footer = () => {
   const footer = document.createElement('footer');
   footer.innerHTML =
     `
-      <h3>Follow Me</h3>
+        <div class="footer-content">
+          <ul>
+            <li>Copyright &copy; 2024 2FH</li>
+            <li><a href="https://github.com/fikrifathulh">2FH GitHub</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#info">Info</a></li>
+            <li><a href="#aboutme">About Me</a></li>
+          </ul>
+        </div>
     `
   return footer;
 };
