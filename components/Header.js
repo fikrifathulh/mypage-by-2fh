88a01c78b96eae2e482c79bc62c389f74d43cc57
fikrifathulh <145629171+fikrifathulh@users.noreply.github.com>
2024-11-22 21:07:09 +0700

    adding class name to nav

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 export const Header = () => {
   const header = document.createElement('header');
   header.innerHTML =
-  `
-    <nav>
+    `
+    <nav class="navbar">
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#info">Info</a></li>
